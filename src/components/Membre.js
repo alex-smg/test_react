699237f59a355979131ccb44e265d19d8566f936
alexandresmagghe <alexandre-smagghe@outlook.com>
2019-03-19 21:42:17 +0100

    end test

--- src/components/Membre.js
+++ src/components/Membre.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const Membre = ({ nom, age, children }) => {
     return(
         <Fragment>
-        <h2 style={{backgroundColor: age > 20 ? '#3F3F3F' : 'red' , color: 'white' }}>{nom.toUpperCase()} : {age}</h2>
+        <h2 style={{backgroundColor: age > 20 ? '#3F3F3F' : 'red' , color: 'white', width: '50%', margin: '2% auto 2% auto' }}>{nom.toUpperCase()} : {age}</h2>
 
             { children ? <p>{children}</p> : <Fragment /> }
 
